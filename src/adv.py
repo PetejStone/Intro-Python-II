9c8a511c83b3b5f13a3c813772bba730798e7532
PetejStone <petejstoney@gmail.com>
2019-09-07 17:35:32 -0700

    setup room and player classes

--- src/adv.py
+++ src/adv.py
@@ -1,6 +1,19 @@
 from room import Room
-
+from player import Player
 # Declare all the rooms
+# class Room:
+#     def __init__(self, name, desc):
+#         self.name = name
+#         self.items = []
+#         self.desc = desc
+      
+
+#     def __str__(self):
+#         return f"{self.name}: {self.items}"
+   
+#     def __repr__(self):
+#         return f"Rooms({repr(self.name)})"
+
 
 room = {
     'outside':  Room("Outside Cave Entrance",
@@ -21,7 +34,7 @@ chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south."""),
 }
 
-
+#print(Room("outside", "it's cold"))
 # Link rooms together
 
 room['outside'].n_to = room['foyer']
@@ -33,6 +46,7 @@ room['narrow'].w_to = room['foyer']
 room['narrow'].n_to = room['treasure']
 room['treasure'].s_to = room['narrow']
 
+print(room['foyer'].e_to)
 #
 # Main
 #
@@ -49,3 +63,43 @@ room['treasure'].s_to = room['narrow']
 # Print an error message if the movement isn't allowed.
 #
 # If the user enters "q", quit the game.
+# outside = Room('outside', 'you are outside')
+# print(outside.name)
+#player = Player('player 1', room['outside'])
+current_room = 'outside'
+quit = True
+while not quit:
+
+    command = input(f"\n(S)elect a Direction ({current_room})\n(Q)uit\n\nCommand: ")
+
+    command = command.lower().strip()  # normalize input
+    
+    if command == '':
+        continue
+
+    command = command[0]
+
+    if command == 'q':  # quit
+        quit = True
+
+    elif command == 's': # select
+        direction = input("Enter list name: ").strip()
+        direction = direction.lower().strip()  # normalize input
+        direction = direction[0]
+
+        room = room[f"{current_room}"]
+        if direction == "n":
+            print(room.n_to)   
+        elif direction == "e":
+            print(room.e_to)
+        elif direction == "s":
+            print(room.s_to)
+        elif direction == "w":
+            print(room.w_to)
+            
+       
+        
+       
+
+    
+      
--- src/player.py
+++ src/player.py
@@ -1,2 +1,15 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+class Player:
+    def __init__(self,player, current_room):
+        self.player = player
+        self.current_room = current_room
+        print(f"Room:{self.current_room}")
+        print(f"Player:{self.player}")
+    
+    def __repr__(self):
+        return f"Room: {repr(self.current_room)}\nPlayer: {repr(self.player)}"
+
+
+
